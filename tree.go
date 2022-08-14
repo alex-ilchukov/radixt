d382bf0f831ba44cd7b27e4f5ac2f5c1fd4444d5
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-08-14 12:35:02 +0300

    #3 Cleanup trailing whitespace characters

--- tree.go
+++ tree.go
@@ -3,8 +3,8 @@ package radixt
 // Tree is the interface for radix tree implementations. Besides the methods
 // below, the implementations should also serve the following contract lines.
 //
-//  1. The trees are static and read-only. That means that their users suppose 
-//     that no new nodes are added or deleted after the users got an instance 
+//  1. The trees are static and read-only. That means that their users suppose
+//     that no new nodes are added or deleted after the users got an instance
 //     of tree. That also means, that trees are safe for concurrent use.
 //  2. Nodes of a tree are named by unique indices. In the documentation below
 //     the indices (named by n parameter) are identified with nodes, that is,
@@ -26,37 +26,37 @@ type Tree interface {
 	// non-node index otherwise.
 	Root() int
 
-	// NodeMark should return ``mark'' of the node n. That is, it should 
-	// return negative number, if the node is not associated with a string 
-	// from original string list, or the index of the associated string 
+	// NodeMark should return ``mark'' of the node n. That is, it should
+	// return negative number, if the node is not associated with a string
+	// from original string list, or the index of the associated string
 	// otherwise.
 	NodeMark(n int) int
 
-	// NodeString should return associated string of the node n or empty 
+	// NodeString should return associated string of the node n or empty
 	// string, if the node is not associated with a string.
 	NodeString(n int) string
 
 	// NodePref should return associated prefix of the node n.
 	NodePref(n int) string
 
-	// NodeEachChild should call func e for every child of the node n, 
+	// NodeEachChild should call func e for every child of the node n,
 	// until e returns boolean true.
-	NodeEachChild(n int, e func(int) bool) 
+	NodeEachChild(n int, e func(int) bool)
 
 	// NodeTransit should implement the transition from node n to the same
 	// node or its child (perhaps, grandchild, grandgrandchild et cetera),
 	// accordingly to the following algorithm. Let start with prefix of the
 	// node n, then
-	//  1) if npos is less than length of the prefix, and byte of the 
+	//  1) if npos is less than length of the prefix, and byte of the
 	//     prefix at npos is b, then the implementation should return n;
-	//  2) if npos is less than length of the prefix, but byte of the 
-	//     prefix at npos is not b, then the implementation should return 
+	//  2) if npos is less than length of the prefix, but byte of the
+	//     prefix at npos is not b, then the implementation should return
 	//     some non-node index;
 	//  3) if npos is more or equal to length of the prefix, then the
 	//     implementation should look for b in first bytes of children
 	//     (grandchildren, grandgrandchildren), skipping all intermediate
 	//     nodes with empty prefixes; if such a child node is found, then
-	//     implementation should return it, otherwise it should return 
+	//     implementation should return it, otherwise it should return
 	//     some non-node index.
 	NodeTransit(n, npos int, b byte) int
 }
