7d3fd1f7611c1d7fd5b82f99c1ffc389cc6b1fc8
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-08-20 17:16:17 +0300

    #12 Adjust documentation of analysis.N struct fields

--- analysis/a.go
+++ analysis/a.go
@@ -3,23 +3,24 @@ package analysis
 // N struct represents data on a node in radix tree in analysis result. Most of
 // the data is return values of Node* methods in [radixt.Tree] interface.
 type N struct {
-	// Index of the node in the tree.
+	// Index is index of the node in the tree.
 	Index int
 
-	// Chunk of the node.
+	// Chunk is chunk of the node.
 	Chunk string
 
-	// Mark of the node.
+	// Mark is mark of the node.
 	Mark uint
 
-	// Index of parent of the node, or non-node index if the node is root.
+	// Parent is index of parent of the node, or non-node index if the node
+	// is root.
 	Parent int
 
-	// Slice of indices of children of the node. Always is sorted by
-	// ascending of the indices.
+	// Children is slice of indices of children of the node. Always is
+	// sorted by ascending of the indices.
 	Children []int
 
-	// Position of chunk in the [A.P] string.
+	// ChunkPos is position of chunk in the [A.P] string.
 	ChunkPos int
 }
 
