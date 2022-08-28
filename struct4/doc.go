c053b5d6a86bb59223079a3d1ed2a4fa3d3d678c
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-08-28 12:37:09 +0300

    #28 Adjust struct4 implementation to the changes of tree interface

--- struct4/doc.go
+++ struct4/doc.go
@@ -4,12 +4,7 @@
 // The implementation is aimed to reduced memory footprint in comparison with
 // generic implementation: The most of node information are contained in just 4
 // bytes. As it provides only limited abilities to store chunks and values of
-// tree nodes, it is not aimed to cover all cases of input data. It has the
-// same additional limitations, as the trees in generic package do.
-
-//  1. Non-empty trees always have zero as their root node.
-//  2. Node indices of non-empty trees go sequentially from zero til their
-//     (Size() - 1) numbers; other numbers are non-node indices.
+// tree nodes, it is not aimed to cover all cases of input data.
 //
 // The package also provides factory method to create a compactified copy of
 // the provided tree. As the tree struct is not exported outside, the
