c0701f1e9c54e8311ec3205791d8cd3e1ce82e84
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-08-30 16:52:08 +0300

    #31 Adjust lookup implementation to the changes of tree interface

--- lookup/l.go
+++ lookup/l.go
@@ -7,10 +7,10 @@ import (
 
 // L contains information on state of the lookup process.
 type L struct {
-	t    radixt.Tree
-	n    uint
-	npos uint
-	stop bool
+	t     radixt.Tree
+	n     uint
+	chunk string
+	stop  bool
 }
 
 // New creates and initializes new lookup state accordingly to the provided
@@ -30,8 +30,18 @@ func New(t radixt.Tree) *L {
 // Reset resets the lookup state.
 func (l *L) Reset() {
 	l.n = 0
-	l.npos = 0
+	l.chunk = l.t.Chunk(0)
 	l.stop = l.t.Size() == 0
+}
+
+func (l *L) try(b byte, n uint, chunk string) bool {
+	l.stop = b != chunk[0]
+	if !l.stop {
+		l.n = n
+		l.chunk = chunk[1:]
+	}
+
+	return l.stop
 }
 
 // Feed takes byte b and returns if the byte is found in radix tree accordingly
@@ -41,52 +51,26 @@ func (l *L) Feed(b byte) bool {
 		return false
 	}
 
-	t := l.t
-	n := l.n
-	byteAt, within := t.ByteAt(n, l.npos)
-	if within {
-		if byteAt == b {
-			l.npos++
-			return true
-		}
-
-		l.stop = true
-		return false
-	}
-
-	for c, f := t.ChildrenRange(n); c <= f; c++ {
-		byteAt, _ := t.ByteAt(c, 0)
-		if byteAt == b {
-			l.n = c
-			l.npos = 1
-			return true
+	if l.chunk != "" {
+		l.try(b, l.n, l.chunk)
+	} else {
+		c, f := l.t.ChildrenRange(l.n)
+		for ; c <= f && l.try(b, c, l.t.Chunk(c)); c++ {
 		}
 	}
 
-	l.stop = true
-	return false
+	return !l.stop
 }
 
 // Found returns if the lookup state points to result string with value in the
 // tree or not.
 func (l *L) Found() bool {
-	if l.stop {
+	if l.stop || l.chunk != "" {
 		return false
 	}
 
-	t := l.t
-	n := l.n
-	_, has := t.Value(n)
-	if !has {
-		return false
-	}
-
-	_, within := t.ByteAt(n, l.npos)
-	if within {
-		return false
-	}
-
-	return true
+	_, has := l.t.Value(l.n)
+	return has
 }
 
 // Tree returns radix tree.
