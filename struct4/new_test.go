c6cf35289d992f5eac4cdc7576ebd635f3610c39
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-01 20:43:28 +0300

    #43 Add tests of factory functions of struct4 implementation

--- struct4/new_test.go
+++ struct4/new_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/alex-ilchukov/radixt"
+	"github.com/alex-ilchukov/radixt/evident"
 	"github.com/alex-ilchukov/radixt/generic"
 	"github.com/alex-ilchukov/radixt/null"
 	"github.com/alex-ilchukov/radixt/struct4"
@@ -70,6 +71,45 @@ func TestNewError(t *testing.T) {
 		_, result2 := struct4.New(tt.tree)
 		if result2 != tt.result2 {
 			t.Errorf(testNewErrorError, i, result2, tt.result2)
+		}
+	}
+}
+
+var newTests = []struct {
+	tree radixt.Tree
+	e    evident.Tree
+}{
+	{tree: nil, e: nil},
+	{tree: emptyOriginal, e: nil},
+	{tree: null.Tree, e: nil},
+	{
+		tree: regularValues,
+		e: evident.Tree{
+			"|": {
+				"GET|0": nil,
+				"P|": {
+					"OST|1": nil,
+					"ATCH|2": nil,
+					"UT|4": nil,
+				},
+				"DELETE|3": nil,
+				"OPTIONS|5": nil,
+				"CONNECT|6": nil,
+				"HEAD|7": nil,
+				"TRACE|8": nil,
+			},
+		},
+	},
+}
+
+const testNewError = "Test New %d: got that New(%v) is\n\n%v\n\nwhich is " +
+	"not equal to\n\n%v\n\n(but should be equal)"
+
+func TestNew(t *testing.T) {
+	for i, tt := range newTests {
+		result, _ := struct4.New(tt.tree)
+		if !tt.e.Eq(result) {
+			t.Errorf(testNewError, i, tt.tree, result, tt.e)
 		}
 	}
 }
