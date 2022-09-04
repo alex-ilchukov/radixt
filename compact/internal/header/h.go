aafaac50c5c7dbc86249023767b1adab79d75cc3
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-04 14:10:22 +0300

    #54 Fix enumeration of node fields in compact/internal/header subpackage

--- compact/internal/header/h.go
+++ compact/internal/header/h.go
@@ -12,37 +12,39 @@ type A8b [hlen]byte
 // following order of node's fields in node bit string:
 //
 //  * head — chunk's position;
-//  * body 0 — value (mogrified);
-//  * body 1 — index of first child (mogrified);
-//  * body 2 — amount of children;
+//  * body 1 — value (mogrified);
+//  * body 2 — index of first child (mogrified);
+//  * body 3 — amount of children;
 //  * tail — chunk's length.
 type H interface {
 	A8b | ~string
 }
+
+const (
+	fieldChunkPos = iota
+	fieldValue
+	fieldChildrenStart
+	fieldChildrenAmount
+	fieldChunkLen
+)
 
 func head[N node.N, Header H](n N, h Header) uint {
 	return node.Head(n, h[0])
 }
 
 func body[N node.N, Header H](n N, h Header, i int) uint {
-	return node.Body(n, h[2*i+1], h[2*i+2])
+	return node.Body(n, h[2*i-1], h[2*i])
 }
 
 func tail[N node.N, Header H](n N, h Header) uint {
 	return node.Tail(n, h[hlen-1])
 }
 
-const (
-	bodyValue = iota
-	bodyStart
-	bodyAmount
-)
-
 // Value takes node n with header h and returns value v of the node with
 // boolean true flag, if the node has value, or default unsigned integer with
 // boolean false otherwise.
 func Value[N node.N, Header H](n N, h Header) (v uint, has bool) {
-	v = body(n, h, bodyValue)
+	v = body(n, h, fieldValue)
 	if v == 0 {
 		return
 	}
@@ -65,11 +67,11 @@ func ChunkRange[N node.N, Header H](n N, h Header) (low, high uint) {
 // and last indices of children of the node, if the node has children, or 1 and
 // 0 otherwise.
 func ChildrenRange[N node.N, Header H](i uint, n N, h Header) (uint, uint) {
-	amount := body(n, h, bodyAmount)
+	amount := body(n, h, fieldChildrenAmount)
 	if amount == 0 {
 		return 1, 0
 	}
 
-	f := body(n, h, bodyStart) + i + 1
+	f := body(n, h, fieldChildrenStart) + i + 1
 	return f, f + amount  - 1
 }
