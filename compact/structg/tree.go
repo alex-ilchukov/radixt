30d0d4999cff1c31c9e385a67d312414c08b22b8
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-05 10:14:56 +0300

    #56 Adjust compact/structg implementation to the changes

--- compact/structg/tree.go
+++ compact/structg/tree.go
@@ -39,14 +39,15 @@ func (t *tree[_]) Chunk(n uint) (c string) {
 	return
 }
 
-// ChildrenRange returns first and last indices of children of node n, if the
-// tree has the node and the node has children, or 1 and 0 otherwise.
-func (t *tree[_]) ChildrenRange(n uint) (uint, uint) {
-	if n >= t.Size() {
-		return 1, 0
+// ChildrenRange returns low and high indices of children of node n, if the
+// tree has the node and the node has children, or default unsigned integers
+// otherwise.
+func (t *tree[_]) ChildrenRange(n uint) (low, high uint) {
+	if n < t.Size() {
+		low, high = header.ChildrenRange(n, t.nodes[n], t.h)
 	}
 
-	return header.ChildrenRange(n, t.nodes[n], t.h)
+	return
 }
 
 var (
