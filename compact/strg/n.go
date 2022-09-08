b1e30ae845236bd2c1261da45c94d39b88e4afcf
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-08 11:13:10 +0300

    #58 Fix amount of bytes in N4 type in compact/strg

--- compact/strg/n.go
+++ compact/strg/n.go
@@ -6,7 +6,7 @@ type N3 [3]byte
 
 // N4 is 4-bytes array and used to select how many bytes per node is used in
 // tree implementation.
-type N4 [3]byte
+type N4 [4]byte
 
 // N represents a set of number of bytes for nodes in tree implementation. As
 // Go doesn't support constants as generic parameters, the types are used
