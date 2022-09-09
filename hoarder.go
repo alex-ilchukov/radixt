04d41aafb5a40b36f5cf690e40298e745f801d04
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-09 15:35:12 +0300

    #60 Shorten declaration of Hoard method in Hoarder interface

--- hoarder.go
+++ hoarder.go
@@ -14,5 +14,5 @@ type Hoarder interface {
 	// Hoard should return amount of bytes, taken by the implementation,
 	// with hint how to interpret the amount (see [HoardExactly] and
 	// [HoardAtLeast]).
-	Hoard() (amount uint, hint uint)
+	Hoard() (amount, hint uint)
 }
