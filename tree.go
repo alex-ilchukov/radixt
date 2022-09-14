2582c1d55255193a12a2a5d5b4fbb7706bd53e88
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-14 09:34:29 +0300

    #70 Relax tree contract
    The following has been removed from the tree contract:
    *   the lines regarding static and read-only nature of the trees;
    *   the lines regarding sequential order of children indices.

--- tree.go
+++ tree.go
@@ -3,21 +3,16 @@ package radixt
 // Tree is the interface for radix tree implementations. Besides the methods
 // below, the implementations should also serve the following contract lines.
 //
-//  1. The trees are static and read-only. That means that their users suppose
-//     that no new nodes are added or deleted after the users got an instance
-//     of tree. That also means, that trees are safe for concurrent use.
-//  2. Nodes of a tree are named by unique indices. In the documentation below
+//  1. Nodes of a tree are named by unique indices. In the documentation below
 //     the indices (named by n parameter) are identified with nodes, that is,
 //     instead of "node with index n" just "node n" expression is used.
-//  3. Node indices of non-empty tree go from zero to (tree size - 1) number in
+//  2. Node indices of non-empty tree go from zero to (tree size - 1) number in
 //     sequential order: 0, 1, ….
-//  4. Non-empty tree always rocks zero as its root.
-//  5. Children indices go in sequential order for any parent node: f, (f + 1),
-//     (f + 2), ….
-//  6. Any child's index is always strictly greater than index of its parent.
-//  7. Empty chunk is allowed only for root nodes of the trees (so any child
+//  3. Non-empty tree always rocks zero as its root.
+//  4. Any child's index is always strictly greater than index of its parent.
+//  5. Empty chunk is allowed only for root nodes of the trees (so any child
 //     node must have non-empty chunk).
-//  8. First bytes of children chunks should be unique over every parent node.
+//  6. First bytes of children chunks should be unique over every parent node.
 //
 // The interface does not put any limitations on values besides its domain of
 // unsigned integers.
