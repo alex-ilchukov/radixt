a9b7780211bda16b1832d0b7d6cd94b9f42cd0ed
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-14 13:06:51 +0300

    #70 Add lines on static nature to documentation of null implementation

--- null/doc.go
+++ null/doc.go
@@ -1,5 +1,6 @@
 // Package null contains an implementation of radix tree accordingly to
 // interface in the parent radixt package. The implementation represents an
 // empty tree and provides an instance of the tree with no factory method. All
-// integer numbers are non-node indices for the tree.
+// integer numbers are non-node indices for the tree. The tree is totally
+// static and safe for use by multiple goroutines concurrently.
 package null
