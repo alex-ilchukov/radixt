63bd799a0d867503bd92b98527e046c14a6af41a
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-15 11:10:14 +0300

    #70 Add lines on static nature to documentation of compact/strg

--- compact/strg/doc.go
+++ compact/strg/doc.go
@@ -5,7 +5,8 @@
 // The implementation is aimed to have reduced memory footprint in comparison
 // with generic implementation: The most of node information are contained in
 // just 3 or 4 bytes. As it provides only limited abilities to store chunks and
-// values of tree nodes, it is not aimed to cover all cases of input data.
+// values of tree nodes, it is not aimed to cover all cases of input data. It
+// is totally static and safe to use by multiple goroutines concurrently.
 //
 // The package also provides factory method to create a compactified copy of
 // the provided tree. Also, the copies could be saved as regular Go strings
