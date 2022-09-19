2e228964ce78596f3d3181ea0513a45a9ea2339f
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-19 10:22:10 +0300

    #74 Implement lookup.Switcher with compact/structg trees

--- compact/structg/tree.go
+++ compact/structg/tree.go
@@ -4,6 +4,7 @@ import (
 	"github.com/alex-ilchukov/radixt"
 	"github.com/alex-ilchukov/radixt/compact/internal/header"
 	"github.com/alex-ilchukov/radixt/compact/internal/node"
+	"github.com/alex-ilchukov/radixt/lookup"
 )
 
 type tree[N node.N] struct {
@@ -68,9 +69,33 @@ func (t *tree[N]) Hoard() (amount, hint uint) {
 	return
 }
 
+// Switch takes node n and byte b. If the node belongs to the tree, it looks
+// for a child c of the node with such a chunk, that its first byte coincides
+// with b. If such a child is found, it returns the child with its chunk
+// without first byte and boolean truth. Otherwise or if the node is not in the
+// tree, it returns corresponding default values.
+func (t *tree[_]) Switch(n uint, b byte) (c uint, chunk string, found bool) {
+	if n >= t.Size() {
+		return
+	}
+
+	for c, h := header.ChildrenRange(n, t.nodes[n], t.h); c < h; c++ {
+		child := t.nodes[c]
+		low := header.ChunkLow(child, t.h)
+		if t.chunks[low] == b {
+			high := low + header.ChunkLen(child, t.h)
+			return c, t.chunks[low+1 : high], true
+		}
+	}
+
+	return
+}
+
 var (
 	_ radixt.Tree    = (*tree[uint32])(nil)
 	_ radixt.Hoarder = (*tree[uint32])(nil)
+	_ lookup.Switcher = (*tree[uint32])(nil)
 	_ radixt.Tree    = (*tree[uint64])(nil)
 	_ radixt.Hoarder = (*tree[uint64])(nil)
+	_ lookup.Switcher = (*tree[uint64])(nil)
 )
