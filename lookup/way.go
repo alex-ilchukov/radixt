0b543967543f29749f3ae6f8f32775ab9bc7787a
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-19 13:25:07 +0300

    #74 Introduce lookup.Switch type of lookup way

--- lookup/way.go
+++ lookup/way.go
@@ -3,3 +3,8 @@ package lookup
 // Default is used in lookup implementation as a parameter type to set up
 // default, non-assisted way of lookup in radix trees.
 type Default [0]Switcher
+
+// Switch is used in lookup implementation as a parameter type to set up a way
+// of lookup in radix trees, which is assisted (and possible sped up) by
+// [Switcher] implementation.
+type Switch [1]Switcher
