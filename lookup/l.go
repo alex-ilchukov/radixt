78bce92c00c7e9920d770fdacf95f0261add5920
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-20 09:16:52 +0300

    #77 Rollback lookup implementation to the pre-generic state

--- lookup/l.go
+++ lookup/l.go
@@ -5,23 +5,12 @@ import (
 	"github.com/alex-ilchukov/radixt/null"
 )
 
-type l[W Way] struct {
+// L contains information on state of the lookup process.
+type L struct {
 	t     radixt.Tree
-	w     W
 	n     uint
 	chunk string
 	keep  bool
-}
-
-// L contains information on state of the lookup process.
-type L struct {
-	l[Default]
-}
-
-// LS contains information on state of the lookup process with assistance of
-// [Switcher] instance.
-type LS struct {
-	l[Switch]
 }
 
 // New creates and initializes new lookup state accordingly to the provided
@@ -32,84 +21,61 @@ func New(t radixt.Tree) *L {
 		t = null.Tree
 	}
 
-	l := &L{l: l[Default]{t: t}}
-	l.Reset()
-
-	return l
-}
-
-// NewInTritcher creates and initializes new lookup state with support of
-// [Switch] lookup way accordingly to the provided radix tree t, and returns a
-// pointer the state. Nil values of t are supported and interpreted as empty
-// tree.
-func NewInTritcher(tr Tritcher) *LS {
-	t := radixt.Tree(tr)
-	if tr == nil {
-		t = null.Tree
-	}
-
-	l := &LS{l: l[Switch]{t: t, w: Switch{tr}}}
+	l := &L{t: t}
 	l.Reset()
 
 	return l
 }
 
 // Reset resets the lookup state.
-func (lkp *l[_]) Reset() {
-	lkp.n = 0
-	lkp.chunk = lkp.t.Chunk(0)
-	lkp.keep = lkp.t.Size() > 0
+func (l *L) Reset() {
+	l.n = 0
+	l.chunk = l.t.Chunk(0)
+	l.keep = l.t.Size() > 0
 }
 
-func (lkp *l[_]) try(b byte, n uint, chunk string) {
-	lkp.keep = b == chunk[0]
-	if lkp.keep {
-		lkp.n = n
-		lkp.chunk = chunk[1:]
+func (l *L) try(b byte, n uint, chunk string) {
+	l.keep = b == chunk[0]
+	if l.keep {
+		l.n = n
+		l.chunk = chunk[1:]
 	}
 }
 
 // Feed takes byte b and returns if the byte is found in radix tree accordingly
 // to the state or not.
-func (lkp *l[W]) Feed(b byte) bool {
+func (l *L) Feed(b byte) bool {
 	switch {
-	case !lkp.keep:
+	case !l.keep:
 		// no statement
-	case lkp.chunk != "":
-		lkp.try(b, lkp.n, lkp.chunk)
-	case IsSwitch[W]():
-		// The trick with len is required to coax the compiler: putting
-		// just 0 would bring its wrath on the case, where the array
-		// has zero length (Default way that is), despite the guardian
-		// IsSwitch check.
-		w := lkp.w[len(lkp.w)-1]
-		lkp.n, lkp.chunk, lkp.keep = w.Switch(lkp.n, b)
+	case l.chunk != "":
+		l.try(b, l.n, l.chunk)
 	default:
-		lkp.t.EachChild(lkp.n, func(c uint) bool {
-			lkp.try(b, c, lkp.t.Chunk(c))
-			return lkp.keep
+		l.t.EachChild(l.n, func(c uint) bool {
+			l.try(b, c, l.t.Chunk(c))
+			return l.keep
 		})
 	}
 
-	return lkp.keep
+	return l.keep
 }
 
 // Found returns if the lookup state points to result string with value in the
 // tree or not.
-func (lkp *l[_]) Found() (found bool) {
-	if lkp.keep && lkp.chunk == "" {
-		_, found = lkp.t.Value(lkp.n)
+func (l *L) Found() (found bool) {
+	if l.keep && l.chunk == "" {
+		_, found = l.t.Value(l.n)
 	}
 
 	return
 }
 
 // Tree returns radix tree.
-func (lkp *l[_]) Tree() radixt.Tree {
-	return lkp.t
+func (l *L) Tree() radixt.Tree {
+	return l.t
 }
 
 // Node returns index of current tree node.
-func (lkp *l[_]) Node() uint {
-	return lkp.n
+func (l *L) Node() uint {
+	return l.n
 }
