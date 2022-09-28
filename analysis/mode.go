0715954d6ec951aa9a2bf59163517ba6b95e02fb
Alexander Ilchukov <alex.ilchukov@gmail.com>
2022-09-28 14:42:08 +0300

    #93 Introduce Firstless type of processing into analysis subpackage

--- analysis/mode.go
+++ analysis/mode.go
@@ -3,3 +3,18 @@ package analysis
 // Default represents default mode of analysis of tree nodes' chunks, when the
 // chunks are analyzed and processed as is.
 type Default [0]byte
+
+// Firstless represents advanced mode of analysis of tree nodes' chunks, when
+// the chunks are analyzed and processed _without their first bytes_. The mode
+// concerns the following areas of analysis report:
+//
+//   - [A.C] field would hold "crammed" chunks without their first bytes;
+//   - [N.Chunk] field would hold the node's chunk without its first byte;
+//   - [N.ChunkPos] field would hold position of [N.Chunk] within of [A.C].
+//
+// The following fields would hold the same values as with [Default] mode:
+//
+//   - [N.ChunkFirst] would hold the first byte of _original_ node's chunk;
+//   - [N.ChunkEmpty] would hold the boolean flag on if the node has empty
+//     original chunk or not.
+type Firstless [1]byte
